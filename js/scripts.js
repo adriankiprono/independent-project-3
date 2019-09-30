01ce32af8702e5dd9b7b740e3f8946286351e374
adriankiprono <tuimuradrian6@gmail.com>
2019-09-30 08:57:54 +0300

    add header delani studio and 2019

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,19 @@ $(document).ready(function() {
     $(".project").click(function() {
         $(".para3").toggle();
     });
+    $(".form").submit(function(){
+      event.preventDefault()
+      var name = document.getElementsByClassName("name").innerHTML=Text;
+      var email = document.getElementsByClassName("email").innerHTML=email;
+      var messege = document.getElementsByClassName("messsage").innerHTML=Text;
+      alert(name);
+    })
+   
     
 
+  });
+  $('#part1').mouseenter(function(){
+    $('.text-within').filter("#text-title").show("slow");
+  }).mouseleave(function(){
+    $('#text-title').hide("slow");
   });
