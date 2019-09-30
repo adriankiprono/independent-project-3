f16d6342b31feeed52b76444c53fc9635ede9b7d
adriankiprono <tuimuradrian6@gmail.com>
2019-09-30 09:51:15 +0300

    add hide function to the what we do images

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,43 @@
 $(document).ready(function() {
     $(".design").click(function() {
-      $(".para1").toggle();      
+      $(".para1").toggle();
+      $(".design").hide();     
+    });    
+    $(".para1").click(function() {
+      $(".design").toggle();
+      $(".para1").hide();     
     });
+
     $(".development").click(function() {
-        $(".para2").toggle();
+      $(".para2").toggle();
+      $(".development").hide();
+     
+        
+
+    });
+
+    $(".para2").click(function() {
+      $(".development").toggle();
+      $(".para2").hide();
     });
     $(".project").click(function() {
-        $(".para3").toggle();
+      $(".para3").toggle();
+      $(".project").hide();
+      
+
+    });
+    $(".para3").click(function() {
+      $(".project").toggle();
+      $(".para3").hide();
+      
+
     });
     $("form").submit(function(){
       event.preventDefault()
       var name = $("#name").val();
       var email =$("#email").val();
       var messege =$("#name").val();
-      alert(name +"  we have received your message. Thank you for reaching out to us." );
+      alert(name  +"  we have received your message. Thank you for reaching out to us." );
     })
    
     
